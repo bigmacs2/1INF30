3ec3c81de1bc0f01c2b2deb1fdd803736c127ed4
andres-melgar <amelgar@pucp.edu.pe>
2025-03-29 15:23:40 -0500

    clase

---
+++ 0682/sesión02.b - POO (clase adicional)/C#/Publicaciones_v03/Publicaciones_v03/Principal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publicaciones_v02
+{
+    public class Principal
+    {
+        static void Main(string[] args)
+        {
+            Publicacion publicacion = new Articulo("A relational model of data for large shared data banks", "Edgard Frank Codd", 1970, "ACM Comm", 13, 6, "junio");
+            publicacion.Referenciar();
+
+            string[] autores = { "Erich Gamma", "Richard Helm", "Ralph Johnson", "John Vlissides"};
+            publicacion = new Libro("Design Patterns: Elements of Reusable Object-Oriented Software", autores, 1994, "1era edición", "Massachussets", "Wesley");
+            publicacion.Referenciar();
+
+            publicacion = new Tesis("Systems of Logic Based on Ordinals", "Alan Turing", 1938, "Tesis de doctorado", "Departamento de Matemáticas", "Universidad de Princeton", "Princeton", "NJ");
+            publicacion.Referenciar();
+        }
+    }
+}
