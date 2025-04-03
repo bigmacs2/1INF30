bb029b62844f94d8d21f70aa07e479bd62ab0812
andres-melgar <amelgar@pucp.edu.pe>
2025-04-03 09:03:42 -0500

    clase

---
+++ 0684/sesión04 - lib java/Publicaciones_v02/src/pe/edu/pucp/inf30/sesiones/publicaciones02/publicaciones/Publicaciones_v02.java
@@ -0,0 +1,26 @@
+package pe.edu.pucp.inf30.sesiones.publicaciones02.publicaciones;
+
+import pe.edu.pucp.inf30.sesiones.publicaciones02.publicaciones.estandardIEEE.Tesis;
+import pe.edu.pucp.inf30.sesiones.publicaciones02.publicaciones.estandardIEEE.Libro;
+import pe.edu.pucp.inf30.sesiones.publicaciones02.publicaciones.estandardIEEE.Articulo;
+
+public class Publicaciones_v02 {
+
+    public static void main(String[] args) {
+        Publicacion publicaciones[] = new Publicacion[3];
+        //esta solución no es elegante
+        //if (imprimirEnAPA)
+        //    publicaciones[0] = new pe.edu.pucp.inf30.sesiones.publicaciones01.publicaciones.estandardAPA.Articulo("A relational model of data for large shared data banks", "Codd, Edgar Frank", 1970, "Communications of the ACM", 13, 6, "junio");
+        //else
+            publicaciones[0] = new Articulo("A relational model of data for large shared data banks", "Codd, Edgar Frank", 1970, "Communications of the ACM", 13, 6, "junio");
+        String autores[]= {"Gamma, Erich", "Helm, Richard", "Johnson, Ralph", "Vlissides, John"};        
+        publicaciones[1] = new Libro("Design Patterns: Elements of Reusable Object-Oriented Software", autores, 1970, "1er edición", "Massachusetts", "Addison-Wesley Professional");
+        publicaciones[2] = new Tesis("A system of logic based on ordinals", "Turing, Alan Mathison", 1938, Clase_de_Tesis.DOCTORADO, "Departamento de Matemáticas", "Universidad de Princeton", "Princeton", "NJ");                
+        
+        for (Publicacion publicacion : publicaciones) {
+            publicacion.referenciar();
+            Publicacion.orden_presentacion++;
+        }
+    }
+
+}
