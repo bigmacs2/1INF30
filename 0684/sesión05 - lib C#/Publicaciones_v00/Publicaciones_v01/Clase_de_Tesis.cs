898ba1b0e40dbcf89c3c800aaa0e79b48cc5cd9b
andres-melgar <amelgar@pucp.edu.pe>
2025-04-06 21:17:52 -0500

    preparación de clase

---
+++ 0684/sesión05 - lib C#/Publicaciones_v00/Publicaciones_v01/Clase_de_Tesis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publicaciones_v01
+{ 
+
+    public enum Clase_de_Tesis { LICENCIATURA, MAESTRIA, DOCTORADO };
+
+}
